5d9006399739f5d9dd218ceecfa06d80d8aaee67
Pedro Leandro Coutin Portuondo <pcoutin@ucsd.edu>
2017-02-23 11:20:18 -0800

    yes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+        $("#likeBtn").click(liked);
+}
+function liked() {
+    ga("send", "event", "like", "click");
+    console.log("liked");
 }
